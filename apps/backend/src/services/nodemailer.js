2fa554d4c401ef83b2a08527a7c25264ee6f50b9
Fabian060 <fabianmijares60@gmail.com>
2026-02-03 15:19:35 -0400

    Update nodemailer transport port to 465 and add TLS configuration

--- apps/backend/src/services/nodemailer.js
+++ apps/backend/src/services/nodemailer.js
@@ -2,12 +2,15 @@ import { createTransport } from 'nodemailer';
 
 const nodemailerService = createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false 
+  }
 });
 
 export default nodemailerService;

0352b22e0c5a17b20e551cbc3eea22bb0c26027e
Fabian Mijares <fabianmijares60@gmail.com>
2026-02-03 14:02:44 -0400

    Remove dotenv config from app.js
    Remove dotenv configuration for environment variables.

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -15,7 +15,6 @@ import authRouter from "./src/modules/auth/auth.routes.js";
 import path from "path";
 import { handler as ssrHandler } from './dist/server/entry.mjs';
 const app = express();
-dotenv.config({ path: '/etc/secrets/.env' });
 
 app.use(cors({ credentials: true, origin: ["http://localhost:4321"] }));
 app.use(express.json());

b9a5c7112a47b95f2e7075e7c99c8fe3b3f0c859
Fabian060 <fabianmijares60@gmail.com>
2026-02-04 10:27:05 -0400

    Refactor endpoint constants and improve error handling in verification process

--- apps/backend/src/config/endpoints.js
+++ apps/backend/src/config/endpoints.js
@@ -1,3 +1,3 @@
-export const endpoint = process.env.NODE_ENV === 'dev'
+export const FRONTEND_ENDPOINT = process.env.NODE_ENV === 'dev'
     ? 'http://localhost:4321'
-    : 'https://secondary-dion-fabian60-fa72cf00.koyeb.app/';
+    : 'https://secondary-dion-fabian60-fa72cf00.koyeb.app';

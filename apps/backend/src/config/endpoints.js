3f674b6ac9e862967c80dfb7a4b5a843307b7657
Fabian060 <fabianmijares60@gmail.com>
2026-02-13 16:46:44 -0400

    Endpoints Koyeb

--- apps/backend/src/config/endpoints.js
+++ apps/backend/src/config/endpoints.js
@@ -1,3 +1,3 @@
 export const endpoint = process.env.NODE_ENV === 'dev'
     ? 'http://localhost:4321'
-    : 'https://eager-lilias-fabian060-d3d1a9b9.koyeb.app';
+    : 'https://fundamental-ingeborg-fabian060-8685e7be.koyeb.app';
